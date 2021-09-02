a50cbe19b28535dafa60a129c380f3afdc9e67d7
Arturas Dunovskis <arturas.dunovskis@abcfitness.com>
2021-09-02 14:52:00 +0300

    Add TODO listing

--- examples/manage-mysql/main.tf
+++ examples/manage-mysql/main.tf
@@ -6,7 +6,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 module "terraform-aws-aurora-manage" {
-  source                          = "../../modules/manage-mysql/"
+  source = "../../modules/manage-mysql/"
   // create users from variable file
   use-local-userlist = true
   //create users from AWS Secret
@@ -16,5 +16,5 @@ module "terraform-aws-aurora-manage" {
     endpoint = "test-aurora-mysql.cluster-cmz3l43pawky.eu-west-1.rds.amazonaws.com:3306"
     username = "root"
     password = "testrunner01"
-}
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,107 @@
+
+variable "mysql-credentials" {
+  description = "Credential for access to MySQL cluster"
+  type        = map(any)
+  default = {
+    endpoint = "localhost:3306"
+    username = "root"
+    password = ""
+  }
+}
+
+variable "create-database" {
+  description = "Create new database ?"
+  type        = bool
+  default     = false
+
+}
+
+variable "new-db-name" {
+  description = "New db name. Leave blank if not required"
+  type        = string
+  default     = ""
+
+}
+
+
+variable "user_hosts" {
+  description = "list of hosts and networks allowed for users. Mapping user and host provided in 'users' variable"
+  type        = map(any)
+  default = {
+    "localhost" = "localhost"
+    "dev"       = "10.10.0.0/255.255.0.0"
+    "vpn"       = "10.50.0.0/255.255.0.0"
+    "any"       = "%"
+    "%"         = "%"
+
+  }
+
+}
+
+variable "roles" {
+  description = "Roles list"
+  type        = list(any)
+  default     = ["dev", "qa"]
+}
+
+variable "roles_priv" {
+  description = "Template for privileges. Mapping user and privileges provided in 'users' variable"
+  type        = map(any)
+  default = {
+    "dev" = ["SELECT", "INSERT", "UPDATE", "DELETE", "CREATE", "DROP", "RELOAD", "PROCESS", "REFERENCES", "INDEX", "ALTER", "SHOW DATABASES", "CREATE TEMPORARY TABLES",
+    "LOCK TABLES", "EXECUTE", "REPLICATION SLAVE", "REPLICATION CLIENT", "CREATE VIEW", "SHOW VIEW", "CREATE ROUTINE", "ALTER ROUTINE", "EVENT", "TRIGGER", "LOAD FROM S3", "SELECT INTO S3"]
+    "qa" = ["SELECT", "EXECUTE", "UPDATE", "DELETE", "CREATE"]
+
+  }
+
+
+}
+
+
+variable "use-local-userlist" {
+  description = "Create users from list in variable ?"
+  type        = bool
+  default     = false
+
+}
+
+# local user list. host and role should be created upper
+variable "users" {
+  description = "Provide users list"
+  type = list(object({
+    username = string
+    host     = string
+    role     = string
+    password = string
+  }))
+  default = [
+    {
+      username = "jdoe6"
+      host     = "vpn"
+      role     = "qa"
+      password = "123456543"
+    },
+    {
+      username = "jdoe5"
+      host     = "%"
+      role     = "qa"
+      password = "9518462"
+    }
+  ]
+}
+
+
+variable "use-aws-secret-userlist" {
+  description = "Create users from AWS Secret manager ?"
+  type        = bool
+  default     = false
+
+}
+
+# Please make sure that user list in secret is in correct format: should be in json like local user list upper
+variable "aws-secret-manager-secrets-name" {
+  description = "Name of AWS Secret manager secret with user list in json"
+  type        = string
+  default     = "mysql-users"
+
+}
