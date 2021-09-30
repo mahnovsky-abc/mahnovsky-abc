b6fe439876a3ec5b433b8d2bc4b354c6d7852add
Arturas Dunovskis <arturas.dunovskis@abcfitness.com>
2021-09-30 23:32:33 +0300

    Fix Error: Failed to download module

--- modules/aurora/main.tf
+++ modules/aurora/main.tf
@@ -3,7 +3,7 @@
 module "terraform-aws-naming" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
-  source      = "git@github.com:abcfinancial2/abc-tfmod-naming-convention.git"
+  source      = "git::https://github.com/abcfinancial2/abc-tfmod-naming-convention.git"
   environment = var.environment
   description = var.description
   Creator     = var.Creator
